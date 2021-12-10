2c65c51e93795e5eaffd104101206bf1a62d8e10
truongduchuy910 <tran.ngoc.huy@sun-asterisk.com>
2021-12-10 23:42:18 +0700

    build luc khuya

--- dist/admin/secure/js/35.65e8c7c0e1df91b73d79.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{577:function(n,t,a){"use strict";a.r(t),a.d(t,"default",(function(){return o}));const o=n=>n.data?n.field.options.find(t=>t.value===n.data).label:null}}]);
